70afc7d9b88707f17af1fa717532656a8aa4b791
Muhamed Zubair Benny <220527636@mycput.ac.za>
2024-07-31 14:36:10 +0200

    Domain, Factory, Factory Test

--- Barbershop-Project3/src/test/java/za/ac/cput/factory/AdminFactoryTest.java
+++ Barbershop-Project3/src/test/java/za/ac/cput/factory/AdminFactoryTest.java
@@ -1,18 +1,38 @@
 package za.ac.cput.factory;
 
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import za.ac.cput.domain.Admin;
 
 import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class AdminFactoryTest {
 
     @Test
+    @Order(1)
     void buildAdmin() {
+        //Build Admin
+        Admin admin = new AdminFactory().buildAdmin("100", "Reanu Keeves", "123");
+        assertNotNull(admin);
+        System.out.println(admin);
     }
 
     @Test
-    void testBuildAdmin() {
+    @Order(2)
+    void buildAdminWithNoAdminId() {
+        //Build Admin without Id
+        Admin admin = new AdminFactory().buildAdmin("Zubair", "456");
+        assertNotNull(admin);
+        System.out.println(admin);
+    }
+
+    @Test
+    @Order(3)
+    void buildWithFail(){
+        Admin admin = new AdminFactory().buildAdmin("", "Duane", "789");
+        assertNotNull(admin);
+        System.out.println(admin);
     }
 }
