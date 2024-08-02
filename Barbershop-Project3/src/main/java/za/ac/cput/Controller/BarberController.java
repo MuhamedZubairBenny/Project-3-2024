10150a35f22f40c6b87d71f8e18262f39de4cbec
imaad <imaad.abr@gmail.com>
2024-08-02 15:10:30 +0200

    BarberController

--- Barbershop-Project3/src/main/java/za/ac/cput/Controller/BarberController.java
+++ Barbershop-Project3/src/main/java/za/ac/cput/Controller/BarberController.java
@@ -4,7 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import za.ac.cput.factory.BarberFactory;
 import za.ac.cput.service.BarberServiceImpl;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Controller
 public class BarberController {
@@ -12,8 +17,15 @@ public class BarberController {
     private BarberServiceImpl service;
     //^For when we start using db connectivity
 
-    @GetMapping("/Barbers")
-    public String showBarbers(){
-        return "barber";
+    @GetMapping("/barbers")
+    public String showBarbers(Model model){
+        model.addAttribute("staff", "Staff");
+        model.addAttribute("barbers", Arrays.asList(
+                BarberFactory.buildBarber("001","Ben", "Zubair", "ben10","beard", "081345678"),
+                BarberFactory.buildBarber("002","prince", "Duane", "darock","fade", "0213457689")
+        ));
+        return "barbers";
     }
+
+
 }
